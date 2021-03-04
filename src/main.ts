4375f8862570d577b0f0b7246d1c23d4cd3670cb
Svante Bengtson <svante.b37@gmail.com>
2021-03-04 12:04:45 +0100

    enable performance monitoring

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,10 @@ Sentry.init({
   dsn: "https://91d516fcee2348da93854140a4a8cdcc@o127465.ingest.sentry.io/5654198",
   release: import.meta.env.VITE_COMMIT_REF?.toString(),
   environment: import.meta.env.VITE_CONTEXT?.toString(),
-  integrations: [new Integrations.BrowserTracing()]
+  integrations: [new Integrations.BrowserTracing({
+    tracingOrigins: ['ropescore.app']
+  })],
+  tracesSampleRate: 1.0
 });
 
 createApp(App).use(router).use(store).mount('#app')
