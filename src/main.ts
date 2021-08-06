a657b4dc4df7fbf196630472cb49d26405b5ccc6
Svante Bengtson <svante@swantzter.se>
2021-08-06 19:49:44 +0000

    Sentry shouldn't swallow errors

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ Sentry.init({
   dsn: 'https://91d516fcee2348da93854140a4a8cdcc@o127465.ingest.sentry.io/5654198',
   release: import.meta.env.VITE_COMMIT_REF?.toString(),
   environment: import.meta.env.VITE_CONTEXT?.toString(),
+  logErrors: true,
   integrations: [new Integrations.BrowserTracing({
     tracingOrigins: ['ropescore.app'],
     routingInstrumentation: Sentry.vueRouterInstrumentation(router)
